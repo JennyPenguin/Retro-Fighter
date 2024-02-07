a1fbb2e34353df231a2415b23ed47bc07756a6b7
JennyPenguin <jennyzhu258@gmail.com>
2024-02-07 10:25:40 -0600

    changed timer

--- Js/utili.js
+++ Js/utili.js
@@ -1,11 +1,11 @@
 function rectangularCollision({ rectangle1, rectangle2 }) {
     return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
         rectangle2.position.x &&
-      rectangle1.attackBox.position.x <=
+        rectangle1.attackBox.position.x <=
         rectangle2.position.x + rectangle2.width &&
-      rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
         rectangle2.position.y &&
-      rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height);
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height);
 }
 
 function determineWinner({ player, enemy, timerId }) {
@@ -20,7 +20,7 @@ function determineWinner({ player, enemy, timerId }) {
     }
 }
 
-let timer = 180;
+let timer = 300;
 let timerId;
 function decreaseTimer() {
     if (timer > 0) {
